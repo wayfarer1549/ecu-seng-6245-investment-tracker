20b124c6d967ab14e81ee1bff3ec90d42b954305
Patrick Philips <jppwriter01@gmail.com>
2020-10-31 18:38:53 -0400

    Registered Investment Services app in the Django Admin interface and added the app to Settings

--- investment_tracker/investmentservices/admin.py
+++ investment_tracker/investmentservices/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SharedInvestment, Bond, InvestmentList, Account
+
+admin.site.register(SharedInvestment)
+admin.site.register(Bond)
+admin.site.register(InvestmentList)
+admin.site.register(Account)
