2b59fcc720cd3c2db858c5fc495ca0c7f389b31d
Patrick Philips <jppwriter01@gmail.com>
2020-10-31 15:07:12 -0400

    Added Account model class

--- investment_tracker/investmentservices/models.py
+++ investment_tracker/investmentservices/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+	'''Overview: Represents a customer's account with the financial brokerage.'''
+
+	# Attributes
+	account_number = models.IntegerField()
+	#account_owner = models.ForeignKey()
+	#advisor = models.ForeignKey()
+	#portfolio
+	cash_balance = models.DecimalField(max_digits=22, decimal_laces=2)
