c5c45b0ec6724678e53841b851f0739f7c2f04e3
Patrick Philips <jppwriter01@gmail.com>
2020-11-03 23:20:39 -0500

    Registered Transaction model in the Django admin interface

--- investment_tracker/investmentservices/admin.py
+++ investment_tracker/investmentservices/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import SharedInvestment, Bond, InvestmentList, Account
+from .models import SharedInvestment, Bond, InvestmentList, Account, Transaction
 
 admin.site.register(SharedInvestment)
 admin.site.register(Bond)
 admin.site.register(InvestmentList)
 admin.site.register(Account)
+admin.site.register(Transaction)
