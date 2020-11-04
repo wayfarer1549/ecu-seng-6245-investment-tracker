0daf9b0892654c7023284f7605378463261bc526
Patrick Philips <jppwriter01@gmail.com>
2020-11-03 20:53:15 -0500

    Added initial List View for shared investments

--- investment_tracker/investmentservices/views.py
+++ investment_tracker/investmentservices/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import SharedInvestment 
 
 # Create your views here.
+
+class SharedInvestmentsListView(ListView):
+	'''A list of all investments in the user's portfolio'''
+
+	model = SharedInvestment
+	template_name = 'investmentservices/shared_investments_list.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		return context
+
