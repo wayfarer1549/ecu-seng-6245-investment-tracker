28b1706fdfc9cd19c73b00f0d7702d7605b85208
Patrick Philips <jppwriter01@gmail.com>
2020-11-07 16:05:24 -0500

    Added pure Python implementation for Investor and Investor tests

---
+++ hw4/Users/Investor.py
@@ -0,0 +1,29 @@
+import User
+from investor_services import Account
+
+class Investor(User):
+	# Overview: Represents an investor of the financial services company
+
+	# Attributes
+	def __init__(self, fn, ln, ssn, pn, st,):
+		super().__init__(fn, ln, ssn, pn, st)
+		self.account_number = ''
+		self.active = False
+
+	# Methods
+
+	def add_account(self, number):
+		# Effects: Opens a new account for this customer
+		# number must be a string of length 8
+		# corresponding to first four letters of client
+		# last name and 4 digits such as the year they became customer
+		# i.e. PHIL2014
+		self.account_number = number # expects a string
+		self.active = True
+
+	def close_account(self):
+		# Effects: Closes this customer's account
+		# Sets active to false
+		# No input parameter, so no input domain to specify
+
+		self.active = False
---
+++ hw4/Users/TestInvestor.py
@@ -0,0 +1,53 @@
+import unittest
+import Investor
+
+class TestInvestor(unittest.TestCase):
+
+	def setUp(self):
+		first_name = 'John'
+		last_name = 'Bunyan'
+		ssn = '123-45-6789'
+		phone_num = '252-989-5555'
+		status = False
+		self.new_investor = Investor(
+			first_name,
+			last_name,
+			ssn,
+			phone_num,
+			status)
+
+	def tearDown(self):
+		self.new_investor.dispose()
+
+	def test_investor_constructor(self):
+		first_name = 'John'
+		last_name = 'Bunyan'
+		ssn = '123-45-6789'
+		phone_num = '252-989-5555'
+		status = False
+		
+		self.assertEqual(
+			self.new_investor.first_name, first_name)
+		self.assertEqual(
+			self.new_investor.last_name, last_name)
+		self.assertEqual(
+			self.new_investor.social_security_number, ssn)
+		self.assertEqual(
+			self.new_investor.phone_number, phone_num)
+		self.assertFalse(
+			self.new_investor.status)
+		self.assertFalse(
+			self.new_investor.active)
+
+	def test_add_account(self):
+		acct_number = self.new_investor.last_name[:4] + str(2020)
+		self.new_investor(acct_number)
+		self.assertEqual(
+			self.new_investor.acct_number, acct_number)
+		self.assertTrue(
+			self.new_investor.active)
+
+	def test_close_account(self):
+		self.new_investor.close_account()
+		self.assertFalse(
+			self.new_investor.active)
