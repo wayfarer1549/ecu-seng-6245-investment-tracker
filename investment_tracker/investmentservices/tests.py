7bbe4b9bbc285e1c0e162775a80d04b87046e2c9
Patrick Philips <jppwriter01@gmail.com>
2020-11-15 13:31:38 -0500

    Began sketching out needed test cases for Account, Bond, SharedInvestment, and Transaction models

--- investment_tracker/investmentservices/tests.py
+++ investment_tracker/investmentservices/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from .models import Account, SharedInvestment, Bond
 
 # Create your tests here.
+class AccountModelTest(TestCase):
+
+	def setUp(self):
+		test_acct = Account.objects.create(
+			account_number='OWEN2020',
+			investment_balance=0.00,
+			cash_balance=1250.00,)
+
+	def test_create_account(self):
+		number = 'OWEN2020'
+		inv_balance = 0.00
+		cash_balance = 1250.00
+
+		self.assertEqual(test_acct.account_number, number)
+		self.assertEqual(test_acct.investment_balance, inv_balance)
+		self.assertEqual(test_acct.cash_balance, cash_balance)
+		self.assertTrue(test_acct.active)
+
+	def test_account_balance(self):
+		pass
+
+	def test_cash_balance(self):
+		pass
+
+	def test_investment_balance(self):
+		pass
+
+	def test_acct_number(self):
+		pass
+
+
+class SharedInvestmentTest(TestCase):
+	pass
+
+class BondTest(TestCase):
+	pass
+
+class TransactionTests(TestCase):
+	pass
+
